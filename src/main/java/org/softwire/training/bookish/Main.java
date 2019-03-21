1dc7d937b248d0e921608a1445fd29047ce71baf
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-21 17:07:17 +0000

    Tried JDBI list

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,10 +1,10 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
@@ -12,8 +12,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
         String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String user = "root";
+        String password = "password";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -26,8 +26,27 @@ public class Main {
         // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
+
         Connection connection = DriverManager.getConnection(connectionString);
 
+
+        String query = "SELECT * " +
+                "FROM bookish.books " +
+                "ORDER BY books.title ASC";
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            String title = rs.getString("title");
+            String author = rs.getString("author");
+            String isbn = rs.getString("isbn");
+            String genre = rs.getString("genre");
+            int ageRating = rs.getInt("age_rating");
+            int numberOfCopies = rs.getInt("number_of_copies");
+            System.out.println(title + "\t" + author +
+                    "\t" + isbn + "\t" + genre +
+                    "\t" + ageRating +
+                    "\t" + numberOfCopies);
+        }
 
 
     }
@@ -41,7 +60,15 @@ public class Main {
 
         Jdbi jdbi = Jdbi.create(connectionString);
 
+        String query = "SELECT * FROM bookish.books ORDER BY books.title ASC";
+        List<Book> books = jdbi.withHandle(handle ->
+                handle.createQuery(query)
+                .mapToBean(Book.class)
+                .list()
+        );
 
-
+        for (Book book : books) {
+            System.out.println(book.getTitle());
+        }
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -2,4 +2,67 @@ package org.softwire.training.bookish.models.database;
 
 public class Book {
 
+    String id;
+    String title;
+    String author;
+    String isbn;
+    String genre;
+    int ageRating;
+    int numberOfCopies;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getAgeRating() {
+        return ageRating;
+    }
+
+    public int getNumberOfCopies() {
+        return numberOfCopies;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setAgeRating(int ageRating) {
+        this.ageRating = ageRating;
+    }
+
+    public void setNumberOfCopies(int numberOfCopies) {
+        this.numberOfCopies = numberOfCopies;
+    }
 }
